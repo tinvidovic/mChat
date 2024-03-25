f03e54bd9af7bcdaff7e51a7a7572d9a384eb334
tinvidovic <tin@vidovic.family>
2024-03-25 16:09:09 +0100

    Implement Message component

--- app/src/main/java/com/nticoding/mchat/MainActivity.kt
+++ app/src/main/java/com/nticoding/mchat/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+
                 }
             }
         }
--- app/src/main/java/com/nticoding/mchat/ui/theme/Type.kt
+++ app/src/main/java/com/nticoding/mchat/ui/theme/Type.kt
@@ -10,7 +10,7 @@ import androidx.compose.ui.unit.sp
 val Typography = Typography(
     bodyMedium = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontWeight = FontWeight.Light,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.5.sp
